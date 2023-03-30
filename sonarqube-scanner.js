1f1b8f198e018937cb810da1dd5a02a88a23cda2
Francesco Talomo <francesco.talomo@deltatre.it>
2023-03-30 15:25:10 +0200

    review sq props for pr workflow

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -11,20 +11,13 @@ if (process.env.PRPullRequestId === "'$(System.PullRequest.PullRequestId)'") {
                 "sonar.projectVersion": process.env.version,
                 "sonar.projectKey": process.env.sonarprojectkey,
                 "sonar.sources": ".",
-                // "sonar.exclusions": "/app/src/**/**/*.test.*, /app/src/**/**/**/*.test.*",
-                // "sonar.tests": "/app/src/",
-                // "sonar.test.inclusions":
-                //   "**/*.test.tsx," + "**/*.test.ts," + "**/*.test.js",
-                // "sonar.typescript.lcov.reportPaths": "/app/coverage/lcov.info",
-                // "sonar.testExecutionReportPaths": "/app/test-report.xml",
                 "sonar.pullrequest.key": process.env.PRPullRequestId,
                 "sonar.pullrequest.branch": process.env.PRSourceBranch,
                 "sonar.pullrequest.base": process.env.PRTargetBranch,
                 "sonar.pullrequest.provider": "vsts",
                 "sonar.pullrequest.vsts.instanceUrl": process.env.CollectionUri,
                 "sonar.pullrequest.vsts.project": process.env.TeamProject,
-                "sonar.pullrequest.vsts.repository": process.env.RepositoryName,
-                "sonar.coverage.exclusions": "/src/components/**/*"
+                "sonar.pullrequest.vsts.repository": process.env.RepositoryName
             },
         },
         () => {
