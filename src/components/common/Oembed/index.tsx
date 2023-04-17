97ce45480352743ab38dc920eee2dc4e7dd752b6
Francesco Talomo <francesco.talomo@deltatre.it>
2023-04-17 17:38:15 +0200

    fixed youtube story part style

--- src/components/common/Oembed/index.tsx
+++ src/components/common/Oembed/index.tsx
@@ -1,3 +1,4 @@
+import {GetSrcFromMarkup} from "@/helpers/storyPartsHelper";
 import {StoryPart} from "@/models/types/storyPart";
 
 type OembedProps = {
@@ -7,13 +8,31 @@ type OembedProps = {
 const Oembed = ({...props}: OembedProps) => {
   const {entity} = props as OembedProps;
   const content = entity.content as any;
+  let src = undefined;
+
+  if (content["provider_name"] && content["provider_name"].toLowerCase() === "youtube") {
+    src = GetSrcFromMarkup(content["html"]);
+  }
 
   return (
     entity && (
       <>
-        <div className="grid grid-cols-1 relative overflow-hidden w-full">
-          <div className="w-full mx-auto flex flex-col items-center" dangerouslySetInnerHTML={{__html: content["html"]}} />
-        </div>
+        {src ?
+          (
+            <div className="grid grid-cols-1 relative overflow-hidden w-full pt-[56.25%]">
+              <iframe
+                allowFullScreen
+                src={src}
+                className="w-full h-full absolute"
+              />
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 relative overflow-hidden w-full">
+              <div className="w-full mx-auto flex flex-col items-center"
+                   dangerouslySetInnerHTML={{__html: content["html"]}}/>
+            </div>
+          )}
+
       </>
     )
   );
---
+++ src/helpers/storyPartsHelper.ts
@@ -0,0 +1,11 @@
+﻿export const GetSrcFromMarkup = (html: string): string => {
+  if (!html) {
+    return "";
+  }
+  const patternRegex = /(?<=src=").*?(?=[\*"])/g;
+  const src = Array.from(html.matchAll(patternRegex)).map(m => m[0]);
+  
+  console.log(src)
+
+  return src.length > 0 ? src[0] : "";
+}
