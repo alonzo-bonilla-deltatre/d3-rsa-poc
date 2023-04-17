6b0c7bb8edf38b54422e77c17e8e4f163d3c26b3
Francesco Talomo <francesco.talomo@deltatre.it>
2023-04-17 17:44:37 +0200

    removed console.log

--- src/helpers/storyPartsHelper.ts
+++ src/helpers/storyPartsHelper.ts
@@ -5,7 +5,5 @@
   const patternRegex = /(?<=src=").*?(?=[\*"])/g;
   const src = Array.from(html.matchAll(patternRegex)).map(m => m[0]);
   
-  console.log(src)
-
   return src.length > 0 ? src[0] : "";
 }
