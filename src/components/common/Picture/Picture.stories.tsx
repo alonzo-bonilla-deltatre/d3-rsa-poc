af38c9274c5b301f84d9462818e7069eb3c68939
Claudia Bertolino <claudia.bertolino@deltatre.it>
2023-04-21 12:01:54 +0200

    Added GadAsset component

--- src/components/common/Picture/Picture.stories.tsx
+++ src/components/common/Picture/Picture.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Picture from "@/components/common/Picture";
+import { transformations } from "@/utilities/cloudinaryTransformations";
 
 const meta: Meta<typeof Picture> = {
   title: 'UiComponents/Picture',
@@ -11,28 +12,27 @@ const meta: Meta<typeof Picture> = {
 export default meta;
 type Story = StoryObj<typeof Picture>;
 
-export const CustomRatio: Story = {
+export const Default: Story = {
   render: (args) => (
     <>
-      <Picture {...args} className="w-full h-full object-cover"/>
+      <Picture {...args} className="w-full h-full object-cover" />
     </>
   ),
   args: {
     src: "https://res.cloudinary.com/forgephotos/image/private/{formatInstructions}/forgego-sandbox/npcnwglmc2lzmqe6uuqh",
-  width: 630,
-  height: 270,
-  alt: "Tokio 2020",
-  transformations: {
-    mobile: "t_ratio21_9-size30",
-    tablet: "t_ratio21_9-size30",
-    desktop: "t_ratio21_9-size50"
-  },
-  className: "w-full h-full object-cover",
+    alt: "Tokio 2020",
+    transformations: {
+      mobile: "t_ratio21_9-size30",
+      tablet: "t_ratio21_9-size30",
+      desktop: "t_ratio21_9-size50",
+      mobileWidth: 630,
+      mobileHeight: 270
+    },
+    className: "w-full h-full object-cover",
   },
   parameters: {
     layout: 'centered',
   },
 };
-
 
 
