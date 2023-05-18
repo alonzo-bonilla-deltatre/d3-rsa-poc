ba33076b6e809fa11416c0c3cc0e413b0d7870b0
Claudia Bertolino <claudia.bertolino@deltatre.it>
2023-05-18 11:45:54 +0200

    commit

--- src/app/metadata.tsx
+++ src/app/metadata.tsx
@@ -1,19 +1,19 @@
 let SeoMetadata = {
-    title: '',
-    description: '3',
-    //metadataBase: '',
-    metadataOpenGraph: {
-      type: 'website',
-      url: 'https://example.com',
-      title: 'My Website',
-      description: 'My Website Description',
-      siteName: 'My Website',
-      images: [
-        {
-          url: 'https://example.com/og.png',
-        },
-      ],
-    },
-  };
-  
-  export { SeoMetadata };
+  title: '',
+  description: '3',
+  //metadataBase: '',
+  metadataOpenGraph: {
+    type: 'website',
+    url: 'https://example.com',
+    title: 'My Website',
+    description: 'My Website Description',
+    siteName: 'My Website',
+    images: [
+      {
+        url: 'https://example.com/og.png',
+      },
+    ],
+  },
+};
+
+export { SeoMetadata };
