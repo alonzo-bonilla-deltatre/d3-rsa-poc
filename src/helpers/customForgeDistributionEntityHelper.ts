db0352cacb41e858fcbb90168abe96c2e4c35c18
Francesco Talomo <Francesco.Talomo@deltatre.com>
2024-05-10 09:31:00 +0200

    Merged PR 137023: 308536: removed forge customization
    # Short description
    
    removed forge customization.
    
    ## Checklist:
    
    - [x] I have performed a self-review of my code
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my change is effective or that my feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] There are no Code Coverage regressions

--- src/helpers/customForgeDistributionEntityHelper.ts
+++ src/helpers/customForgeDistributionEntityHelper.ts
@@ -1,6 +1,5 @@
 /* istanbul ignore file */
-import { getSingleAssetByTag } from '@/services/gadService';
-import { DistributionEntity, ForgeDapiEntityCode, ForgeEntityCode, ForgeEntityType } from '@/models/types/forge';
+import { DistributionEntity, ForgeDapiEntityCode, ForgeEntityType } from '@/models/types/forge';
 import { getEntity } from '@/services/forgeDistributionService';
 import { LinkRuleVariation } from '@/models/types/linkRule';
 
@@ -16,19 +15,20 @@ export const customEnrichLinkRuleRequestEntity = (
   entities: DistributionEntity[],
   linkRuleVariations?: LinkRuleVariation[]
 ) => {
-  if (entities?.length > 0) {
-    entities.forEach((entity) => {
-      if (entity.type === ForgeEntityType.customEntity && entity.entityCode === ForgeEntityCode.event) {
-        entity['eventslug'] = entity.slug;
-      } else {
-        entity.tags?.forEach((tag) => {
-          if (tag.externalSourceName === `${ForgeEntityType.customEntity}.${ForgeEntityCode.event}`) {
-            entity['eventslug'] = tag.slug;
-          }
-        });
-      }
-    });
-  }
+  // Add here your custom code to enrich the entities with a link rule request entity.
+  // if (entities?.length > 0) {
+  //   entities.forEach((entity) => {
+  //     if (entity.type === ForgeEntityType.customEntity && entity.entityCode === ForgeEntityCode.event) {
+  //       entity['eventslug'] = entity.slug;
+  //     } else {
+  //       entity.tags?.forEach((tag) => {
+  //         if (tag.externalSourceName === `${ForgeEntityType.customEntity}.${ForgeEntityCode.event}`) {
+  //           entity['eventslug'] = tag.slug;
+  //         }
+  //       });
+  //     }
+  //   });
+  // }
 };
 
 /**
@@ -41,9 +41,10 @@ export const customEnrichLinkRuleRequestEntity = (
 export const customEnrichEntitiesWithGadAssetsFields = async (
   entities: DistributionEntity[]
 ): Promise<DistributionEntity[]> => {
-  for (const entity of entities) {
-    await customEnrichEntitiesWithGadAssetsFieldsForPlayers(entity);
-  }
+  // Add here your custom code to enrich the entities with GAD assets fields.
+  // for (const entity of entities) {
+  //   await customEnrichEntitiesWithGadAssetsFieldsForPlayers(entity);
+  // }
   return entities;
 };
 
@@ -60,15 +61,10 @@ export const customEnrichEntitiesWithReferencesFields = async (
   const result = [];
   for (let entity of entities) {
     let tempEntity = null;
-    if (entity.entityCode === ForgeEntityCode.match) {
-      tempEntity = await customEnrichEntitiesWithReferencesFieldsForMatches(tempEntity, entity);
-    }
-    if (entity.type === ForgeEntityType.story) {
-      tempEntity = await customEnrichEntitiesWithReferencesFieldsForStories(tempEntity, entity);
-    }
-    if (entity.type === ForgeEntityType.customEntity && entity.entityCode === ForgeEntityCode.event) {
-      tempEntity = await customEnrichEntitiesWithReferencesFieldsForEvents(tempEntity, entity);
-    }
+    // Add here your custom code to enrich the entities with references fields.
+    // if (entity.type === ForgeEntityType.story) {
+    //   tempEntity = await customEnrichEntitiesWithReferencesFieldsForStories(tempEntity, entity);
+    // }
     if (tempEntity) {
       result.push(tempEntity);
     } else {
@@ -102,87 +98,4 @@ export const customEnrichAlbumListWithElementCount = async (
     }
   }
   return result;
-};
-
-/**
- * Enriches the given entity with GAD assets fields for players.
- *
- * @async
- * @param {DistributionEntity} entity - The entity to enrich.
- */
-const customEnrichEntitiesWithGadAssetsFieldsForPlayers = async (entity: DistributionEntity) => {
-  if (entity.entityCode === ForgeEntityCode.player && entity?.fields?.playerNationalityFlag) {
-    entity.fields.playerNationalityFlag = await getSingleAssetByTag(entity.fields.playerNationalityFlag);
-  }
-};
-
-/**
- * Enriches the given entity with references fields for matches.
- *
- * @async
- * @param {DistributionEntity | null} tempEntity - The temporary entity to enrich.
- * @param {DistributionEntity} entity - The entity to enrich.
- * @returns {Promise<DistributionEntity>} The enriched entity.
- */
-const customEnrichEntitiesWithReferencesFieldsForMatches = async (
-  tempEntity: DistributionEntity | null,
-  entity: DistributionEntity
-): Promise<DistributionEntity> => {
-  tempEntity = await getEntity(ForgeDapiEntityCode.matches, entity.slug);
-  if (
-    tempEntity?.references &&
-    tempEntity?.references?.teamHome?.length > 0 &&
-    tempEntity.references.teamHome[0]?.fields?.teamLogo &&
-    tempEntity?.references?.teamAway?.length > 0 &&
-    tempEntity.references.teamAway[0]?.fields?.teamLogo
-  ) {
-    tempEntity.references.teamHome[0].fields.teamLogo = tempEntity?.references?.teamHome[0].fields?.teamLogo
-      ? await getSingleAssetByTag(tempEntity?.references?.teamHome[0].fields?.teamLogo)
-      : null;
-    tempEntity.references.teamAway[0].fields.teamLogo = tempEntity?.references?.teamAway[0].fields?.teamLogo
-      ? await getSingleAssetByTag(tempEntity?.references?.teamAway[0].fields?.teamLogo)
-      : null;
-  }
-  return tempEntity as DistributionEntity;
-};
-
-/**
- * Enriches the given entity with references fields for stories.
- *
- * @async
- * @param {DistributionEntity | null} tempEntity - The temporary entity to enrich.
- * @param {DistributionEntity} entity - The entity to enrich.
- * @returns {Promise<DistributionEntity>} The enriched entity.
- */
-const customEnrichEntitiesWithReferencesFieldsForStories = async (
-  tempEntity: DistributionEntity | null,
-  entity: DistributionEntity
-): Promise<DistributionEntity> => {
-  tempEntity = await getEntity(ForgeDapiEntityCode.stories, entity.slug);
-  if (
-    tempEntity?.references &&
-    tempEntity?.references?.sponsoredBy?.length > 0 &&
-    tempEntity.references.sponsoredBy[0]?.fields?.partnerLogo
-  ) {
-    tempEntity.references.sponsoredBy[0].fields.partnerLogo = tempEntity?.references?.sponsoredBy[0].fields?.partnerLogo
-      ? await getSingleAssetByTag(tempEntity?.references?.sponsoredBy[0].fields?.partnerLogo)
-      : null;
-  }
-  return tempEntity as DistributionEntity;
-};
-
-/**
- * Enriches the given entity with references fields for events.
- *
- * @async
- * @param {DistributionEntity | null} tempEntity - The temporary entity to enrich.
- * @param {DistributionEntity} entity - The entity to enrich.
- * @returns {Promise<DistributionEntity>} The enriched entity.
- */
-const customEnrichEntitiesWithReferencesFieldsForEvents = async (
-  tempEntity: DistributionEntity | null,
-  entity: DistributionEntity
-): Promise<DistributionEntity> => {
-  tempEntity = await getEntity(ForgeDapiEntityCode.events, entity.slug);
-  return tempEntity as DistributionEntity;
 };
