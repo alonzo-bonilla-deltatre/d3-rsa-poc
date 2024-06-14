3cb95f91859296a0e02375172968d50ff3ab0768
Francesco Talomo <Francesco.Talomo@deltatre.com>
2024-06-14 12:29:39 +0000

    fix table typography

--- src/components/commons/Table/Table.tsx
+++ src/components/commons/Table/Table.tsx
@@ -1,5 +1,6 @@
 import { StoryPart } from '@/models/types/storyPart';
 import HtmlContent from '@/components/commons/HtmlContent/HtmlContent';
+import Typography from '@/components/commons/Typography/Typography';
 
 type TableProps = {
   entity?: StoryPart;
@@ -12,7 +13,7 @@ const Table = ({ entity }: TableProps) => {
   const body = content['body'];
   return (
     <div className="grid grid-cols-1 relative overflow-hidden w-full">
-      {title && <div className={'d3-ty-heading-6 font-bold mb-4'}>{title}</div>}
+      {title && <Typography variant={'h6'} className={'mb-4'}>{title}</Typography>}
       {body && <HtmlContent content={body} />}
     </div>
   );
